6de5128b615c64919090ec28f6abcd22cda59f8c
vijit1018 <vijittiwari1018@gmail.com>
2025-03-28 11:26:04 +0530

    Fixed DB_HOST

--- db.js
+++ db.js
@@ -7,7 +7,8 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT, 
-    ssl: { rejectUnauthorized: false } // Ensure secure connection
+    ssl: { rejectUnauthorized: false }, // Ensure secure connection
+    connectTimeout: 20000
 });
 
 db.connect((err) => {
